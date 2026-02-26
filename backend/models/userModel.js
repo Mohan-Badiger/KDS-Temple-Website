c9e825e24627c4f375bef196111c915a93cac4a8
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-02-26 14:19:42 +0530

    removed commented lines in userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,48 +1,8 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-//     resetOtp: { type: String },          // OTP for reset password
-//     resetOtpExpiry: { type: Date },      // OTP expiry time
-// });
-
-// // Prevent model overwrite on hot reloads in dev
-// const userModel = mongoose.models.user || mongoose.model('user', userSchema);
-
-// export default userModel;
-
-//===================================================================================================================
-
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//   name: { type: String },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String },
-
-//   // For email verification
-//   verifyOtp: { type: String },
-//   verifyOtpExpiry: { type: Number },
-//   isVerified: { type: Boolean, default: false },
-
-//   // For password reset
-//   resetOtp: { type: String },
-//   resetOtpExpiry: { type: Date },
-// });
-
-// const userModel = mongoose.models.user || mongoose.model("user", userSchema);
-// export default userModel;
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, trim: true },
-
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-
   password: { type: String },
 
   // For email verification
