e5131388d5b7ff957299d8eb15b260a145f8aca9
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-04-14 12:56:50 +0530

    removed test2 file

--- backend/test2.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-http.get('http://localhost:4000/api/user/profile', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => fs.writeFileSync('response.txt', res.statusCode + ' ' + data));
-}).on('error', err => fs.writeFileSync('response.txt', 'ERR: ' + err.message));
